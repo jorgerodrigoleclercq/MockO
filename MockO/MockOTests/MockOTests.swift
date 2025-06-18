e824e1707296cd7a0dddb39a1d55bf3a880d77f0
JorgeRodrigoLeclercq <jorge.rodrigoleclercq@datadoghq.com>
2025-06-18 12:51:03 +0200

    Add source code

---
+++ MockO/MockOTests/MockOTests.swift
@@ -0,0 +1,17 @@
+//
+//  MockOTests.swift
+//  MockOTests
+//
+//  Created by Jorge Rodrigo Leclercq on 18/6/25.
+//
+
+import Testing
+@testable import MockO
+
+struct MockOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
